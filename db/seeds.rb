d984796f0d461660b8aaa72dc12552cea13a9eab
Shawn Peng <shawn-peng@outlook.com>
2019-07-27 10:48:17 +0800

    Seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Story.destroy_all if Rails.env.development?
+
+5.times do
+  story = Story.new
+  story.name = Faker::TvShows::GameOfThrones.character
+  story.text = Faker::TvShows::GameOfThrones.quote
+  story.save
+end
+
+3.times do
+  comment = Comment.new
+  comment.story_id = rand(1..Story.count)
+  comment.name = Faker::TvShows::GameOfThrones.character
+  comment.content = Faker::TvShows::GameOfThrones.quote
+  comment.save
+end
